c9edcc8189792612f4f2d18f2c273da03a17d0cb
AdeThorMiwa <BenDaMyth@gmail.com>
2021-07-03 13:00:27 +0100

    updating git repository

--- tests/index.js
+++ tests/index.js
@@ -1,42 +1,20 @@
+const QueryBuilder = require("./../dist/index.js");
 
-// (async () => {
-//   // console.log(await wallet.createWallet())
-//   // console.log(await wallet.getBalance({ address: "0xb8c40564d412bc4a0a353b201a80f9122cbf81fb", token: TokenTypes.USDT }))
-//   console.log(await wallet.send({
-//     from: "0xb8c40564d412bc4a0a353b201a80f9122cbf81fb",
-//     to: "0x2B1BBEaC89C35FB9C5cbe9bF99B90E2897211B17",
-//     amount: "100",
-//     privKey: "97ab4556c6aec0e1a162c9eca5759c032a6ae989c092824aec9a4b670612b519",
-//     token: TokenTypes.USDT
-//   }))
-// })();
+console.log(QueryBuilder);
 
+const qb = new QueryBuilder.default({
+  firstname: "Ade",
+  lastname: "Tom",
+  select: "name,type,body",
+  sort: "-createdAt, name",
+  spaceUsed: { gt: 10 },
+});
 
-//
-// const hdAccount = {
-//   address: "0xb8c40564d412bc4a0a353b201a80f9122cbf81fb",
-//   checkSumAddress: "0xb8C40564d412bc4A0A353b201a80F9122cbF81fb",
-//   publicKey:
-//     "030b8b6812c652471f8ee779c8a93176724181df83826baa98fd089aa87ae9eb30",
-//   privateKey:
-//     "ce2b68765cd6e1ea2d49c1db3d12dd0533ee31ea5bc4fb0d10915c15e65ef036",
-//   mnemonics:
-//     "fee clog session woman crack tell guess sister army oil rally rude",
-// };
-//
-// const _wallet = {
-//   address: "0x2B1BBEaC89C35FB9C5cbe9bF99B90E2897211B17",
-//   privateKey:
-//     "0x6bf5106c9c52fafaad55b9d852ea8af4b7d9a647fbe2f0d1f35a65b3d805b78b",
-//   mnemonics:
-//     "sunset vibrant ski bottom debris plug bone bar glass ribbon remember notable",
-// };
-
-// address: '0x32a25308d529f0fc66e2f343ff042987c1859e28',
-// checkSumAddress: '0x32A25308d529f0fc66e2F343Ff042987C1859E28',
-// publicKey:
-//  'd48e4999c28eb1d2cbd668cf116c08e55b5b4cf064354880693ad992b9d6983cdc04137106c5417174a89c8da686f2ef3141e948521b485d66a0737714b8444b',
-// privateKey:
-//  'c527741c6e746e4902df931b1e0c41e9a9a6eb418b1fcc18d9fe03e5877c21f7',
-// mnemonics:
-//  'version truly pull genius animal crime repair stage income sock pumpkin reject' }
+console.log(
+  qb
+    .filter()
+    .paginate()
+    .select()
+    .select()
+    .build()
+);
